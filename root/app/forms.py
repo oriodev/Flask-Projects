bcad2cf66537fad5cb5f526c41d7e6b2cc63d50d
ori <93719767+oriodev@users.noreply.github.com>
2024-02-17 15:36:42 +0000

    remove root

--- root/app/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class FriendForm(FlaskForm):
-  name = StringField('name', validators=[DataRequired()])
-  submit = SubmitField('submit application')
--- root/app/models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Friend(db.Model):
-  __tablename__ = 'friends'
-  friend_id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(20), nullable=False, unique=False, index=True)
-  
-  def __repr__(self):
-    return f"friend(`{self.name}`)"
